2443cf3ff09d299afc9c2ca9d4de671715073592
HamdanRaza309 <githubuse309@gmail.com>
2025-05-04 01:01:00 +0500

    Basic frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from "react"
+import prism from 'prismjs'
+import "prismjs/themes/prism-tomorrow.css";
+import Editor from 'react-simple-code-editor'
+import './App.css'
+
+function App() {
+
+  const [code, setCode] = useState(`function name(params) {
+    return 1 + 1
+  }`)
+
+  useEffect(() => {
+    prism.highlightAll()
+  })
+
+
+
+  return (
+    <>
+      <main>
+        <div className="left">
+          <div className="code">
+            <Editor
+              value={code}
+              onValueChange={code => setCode(code)}
+              highlight={code => prism.highlight(code, prism.languages.javascript, 'javascript')}
+              padding={10}
+              style={{
+                fontFamily: '"Fira code", "Fira Mono", monospace',
+                fontSize: 16,
+                border: '1px solid #ddd',
+                borderRadius: "5px",
+                height: '100%',
+                width: '100%'
+              }}
+            />
+          </div>
+          <div className="review">Review</div>
+        </div>
+        <div className="right"></div>
+      </main>
+    </>
+  )
+}
+
+export default App
