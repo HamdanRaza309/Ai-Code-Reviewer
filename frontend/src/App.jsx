16c05de615e59cac53968d01d65758f9c0121fc8
HamdanRaza309 <githubuse309@gmail.com>
2025-05-04 01:32:07 +0500

    Used rehype-highlight for highlighting our response data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import "prismjs/themes/prism-tomorrow.css";
 import Editor from 'react-simple-code-editor'
 import './App.css'
 import axios from 'axios'
+import Markdown from 'react-markdown'
+import rehypeHighlight from 'rehype-highlight'
+import 'highlight.js/styles/github-dark.css'
 
 function App() {
 
@@ -11,10 +14,13 @@ function App() {
     return 1 + 1
   }`)
 
+  const [review, setReview] = useState(``)
+
   async function reviewCode() {
     const res = await axios.post(`http://localhost:3000/ai/code-review`, { code })
 
     console.log(res.data.response);
+    setReview(res.data.response)
 
   }
 
@@ -44,7 +50,13 @@ function App() {
           </div>
           <div onClick={reviewCode} className="review">Review</div>
         </div>
-        <div className="right"></div>
+        <div className="right">
+          <Markdown
+            rehypePlugins={[rehypeHighlight]}
+          >
+            {review}
+          </Markdown>
+        </div>
       </main>
     </>
   )
